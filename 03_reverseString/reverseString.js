172d40839fb0552905f02d72f3c37194029c8df3
Peace <peacemastre@gmail.com>
2022-11-28 18:06:51 +0100

    completed Exercise 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitStr = string.split("");
+    reverseStr = splitStr.reverse();
+    joinStr = reverseStr.join("");
+    return joinStr
 };
 
 // Do not edit below this line
