28854fce1dd3aab386c00335cf40c89a81a43faa
Peace <peacemastre@gmail.com>
2022-11-29 17:18:10 +0100

    completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = true;
+    if(year % 4 == 0){
+        return isLeapYear
+    } else if(year % 100 == 0 && year % 400 ==0){
+        return isLeapYear
+    }else{
+        isLeapYear = false;
+    }
+    return isLeapYear
 };
 
 // Do not edit below this line
