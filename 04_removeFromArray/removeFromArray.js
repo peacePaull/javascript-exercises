e4c1ca78c69ba381a56a8e7e1cae58093b7d1df8
Peace <peacemastre@gmail.com>
2022-11-29 15:33:06 +0100

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    for(let item of itemsToRemove){
+        let index = array.indexOf(item)
+        if(index >= 0){
+            array.splice(index,1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
