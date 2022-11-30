ebb2b28cde99253e3067823d2c3fc4b3ea168aea
Peace <peacemastre@gmail.com>
2022-11-30 13:36:07 +0100

    completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let converted = (temp - 32) * 5/9;
+  let result = Math.round(converted);
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let converted = (temp * 9/5) + 32;
+  let result = Math.round(converted);
+  return result;
 };
 
 // Do not edit below this line
