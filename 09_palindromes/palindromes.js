7e6ed94df1dd8d061fdd7717fd15784975673691
Peace <peacemastre@gmail.com>
2022-12-02 13:58:15 +0100

    completed exercise 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let reg = /[\W_]/g;
+    let smallStr = str.toLowerCase().replace(reg, "");
+    let reversed = smallStr.split("").reverse().join("");
 
+    if (reversed === smallStr) return true;
+
+    return false;
 };
 
 // Do not edit below this line
